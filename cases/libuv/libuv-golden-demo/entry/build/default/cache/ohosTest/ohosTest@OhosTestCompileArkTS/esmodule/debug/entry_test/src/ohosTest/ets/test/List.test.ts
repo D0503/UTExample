4e1b9ab51950b265fcc423e1c54c7732b42b18bb
duan <1786057218@qq.com>
2026-07-28 10:46:18 +0800

    删除安装依赖

--- cases/libuv/libuv-golden-demo/entry/build/default/cache/ohosTest/ohosTest@OhosTestCompileArkTS/esmodule/debug/entry_test/src/ohosTest/ets/test/List.test.ts
+++
@@ -1,8 +0,0 @@
-import LibuvLoadSuite from "@bundle:com.thirdparty.validation.libuv.golden/entry_test/ets/test/LibuvLoadSuite.test";
-import LibuvCoreSuite from "@bundle:com.thirdparty.validation.libuv.golden/entry_test/ets/test/LibuvCoreSuite.test";
-import LibuvAffinitySuite from "@bundle:com.thirdparty.validation.libuv.golden/entry_test/ets/test/LibuvAffinitySuite.test";
-export default function testsuite() {
-    LibuvLoadSuite();
-    LibuvCoreSuite();
-    LibuvAffinitySuite();
-}
--- cases/tinyxml2/harmony-demo/entry/build/default/cache/ohosTest/ohosTest@OhosTestCompileArkTS/esmodule/debug/entry_test/src/ohosTest/ets/test/List.test.ts
+++
@@ -1,6 +0,0 @@
-import Tinyxml2LoadSuite from "@bundle:com.thirdparty.validation.tinyxml2/entry_test/ets/test/Tinyxml2LoadSuite.test";
-import Tinyxml2CoreSuite from "@bundle:com.thirdparty.validation.tinyxml2/entry_test/ets/test/Tinyxml2CoreSuite.test";
-export default function testsuite() {
-    Tinyxml2LoadSuite();
-    Tinyxml2CoreSuite();
-}
